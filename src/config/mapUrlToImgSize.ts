1763f248b8d5a9ed6666f429fc7fd0a41e3a0748
98lxh <582658211@qq.com>
2022-06-18 14:16:47 +0800

    feat:add images

--- src/config/mapUrlToImgSize.ts
+++ src/config/mapUrlToImgSize.ts
@@ -2,20 +2,20 @@ const baseUrl = '/images/'
 
 export const mapImgUrlToSize = {
   [`${baseUrl}0.jpeg`]: {
-    width: 500,
-    height: 750
+    width: 430,
+    height: 700
   },
   [`${baseUrl}1.jpeg`]: {
-    width: 500,
-    height: 750
+    width: 515,
+    height: 740
   },
   [`${baseUrl}2.jpeg`]: {
-    width: 500,
-    height: 333
+    width: 1040,
+    height: 730
   },
   [`${baseUrl}3.jpeg`]: {
-    width: 500,
-    height: 750
+    width: 582,
+    height: 735
   },
   [`${baseUrl}4.jpeg`]: {
     width: 500,
