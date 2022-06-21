ef6dfbcfaa0b4d43f0f8b4c6ac63629e2981fdca
98lxh <582658211@qq.com>
2022-06-21 11:40:14 +0800

    feat:reMount article list

--- src/components/listContainer/index.tsx
+++ src/components/listContainer/index.tsx
@@ -1,7 +1,8 @@
+import { Timeout } from "ahooks/lib/useRequest/src/types";
 import ArticleList from "components/articleList";
 import Navigation from "components/navigation";
 import BackTop from "libs/backTop";
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { ICategory } from "types/category";
 
 const ArticleListContainer: FC<{ categorys: ICategory[], category?: string, keyword?: string }> = ({
@@ -10,6 +11,22 @@ const ArticleListContainer: FC<{ categorys: ICategory[], category?: string, keyw
   category
 }) => {
   const scrollElement = useRef<HTMLDivElement>(null)
+  const [isMountList,setIsMountList] = useState(true)
+  const timer = useRef<Timeout | null>(null)
+  useEffect(() => { 
+    setIsMountList(false)
+    timer.current = setTimeout(() => { 
+      setIsMountList(true)
+    })
+
+    return () => { 
+      if (timer.current) clearTimeout(timer.current)
+    }
+  }, [
+    category,
+    keyword
+  ])
+
   return (
     <div
       className="h-full overflow-auto bg-white  dark:bg-zinc-800 duration-500 transition-colors scrollbar-thin scrollbar-thumb-zinc-200 dark:scrollbar-thumb-zinc-900"
@@ -18,10 +35,10 @@ const ArticleListContainer: FC<{ categorys: ICategory[], category?: string, keyw
       <Navigation
         categorys={categorys}
       />
-      <ArticleList
+      {isMountList && <ArticleList
         category={category}
         search={keyword}
-      />
+      />}
       <BackTop
         target={scrollElement.current}
       />
